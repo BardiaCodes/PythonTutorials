517bd6e4bc50476bdd80ce2b14d3dea96348d6c2
BardiaSafari <43256864+BardiaCodes@users.noreply.github.com>
2019-04-01 17:24:52 -0700

    Add files via upload

---
+++ tutorial.py
@@ -0,0 +1,25 @@
+# This is a comment.
+
+# This does this.
+
+
+#print('Hello world! print(hello)')
+
+
+"This is a string"
+
+# These are numbers 1,2,3,4,5
+# Integer = negative or positive whole number. NO DECIMAL PLACES!
+# FLOAT = any number
+
+# 12, integer.
+# 16.77, float.
+
+johns_info = 'Johnny is 5 years old and he likes to eat little cheese particles.'
+
+#RULES FOR VARIABLES:
+# Cannot start with a number. No special characters, no spaces. Try to keep it simple.
+
+print(johns_info)
+print('johns_info')
+
